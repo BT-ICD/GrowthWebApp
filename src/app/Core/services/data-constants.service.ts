a9e4f1e9515306c447f8519b85fa88ac54ddd24f
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-01-18 17:19:00 +0530

    remove additiona menu items

--- src/app/Core/services/data-constants.service.ts
+++ src/app/Core/services/data-constants.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataConstantsService {
+  //Local IIS .NET
   readonly BASEAPIURL:string ='https://localhost:44330/api/';
-
+  //Deployed on IIS of Local Machine for ICD Students live session data
+  // readonly BASEAPIURL:string ='http://localhost:7460/API/';
   constructor() { }
 }
