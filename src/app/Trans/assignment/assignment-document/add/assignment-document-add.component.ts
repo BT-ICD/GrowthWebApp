1ee89824f48a3b261ed6ae7baaf15a217a9e770e
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-02-18 18:43:52 +0530

    Allowed to upload doc and pdf files

--- src/app/Trans/assignment/assignment-document/add/assignment-document-add.component.ts
+++ src/app/Trans/assignment/assignment-document/add/assignment-document-add.component.ts
@@ -1,9 +1,9 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {   Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FileUpload } from 'primeng/fileupload';
 
 import { IDocumentTypeLookup } from 'src/app/Core/types/common-types';
 import { AssignmentDocumentDataService } from '../assignment-document-data.service';
-import { IAssignmentDocAdd, IAssignmentDocumentDTODetail } from '../assignment-document-types';
+import {  IAssignmentDocumentDTODetail } from '../assignment-document-types';
 
 @Component({
   selector: 'app-assignment-document-add',
@@ -24,7 +24,11 @@ export class AssignmentDocumentAddComponent implements OnInit {
  notes:string;
  fileData:File;
 
+ //To get referecen of fileupload control to clear existing selected file
+ @ViewChild('pfUpload') pfUpload:ElementRef;
   constructor(private assignmentDocumentDataService:AssignmentDocumentDataService) { }
+ 
+ 
 
   ngOnInit(): void {
     
@@ -33,6 +37,13 @@ export class AssignmentDocumentAddComponent implements OnInit {
     this.fileData = <File>evetData.files[0];
   }
   onShow(){
+    this.notes='';
+    this.documentTypeId=-1;
+    //To clear selected file - if any existing
+    let fUpload = this.pfUpload as unknown as FileUpload;
+    fUpload.clear();
+    
+    
    this.onDialogShow.emit();
   }
   onHide(){
